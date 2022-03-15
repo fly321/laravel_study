36e983f2c8e40ea22b1fc51e3da0a27b81225853
aimer <15267840622@163.com>
2022-03-15 23:55:08 +0800

    route 别名

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -11,7 +11,7 @@ class TaskController extends Controller
     }
 
     public function url(){
-        return route('task.index',['id'=>10]);
+        return redirect()->route('task.index',['id'=>10]);
     }
 
 }
