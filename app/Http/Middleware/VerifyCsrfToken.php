725b828b4e24cfa50d00df44563cd35075c5d4de
aimer <15267840622@163.com>
2022-03-17 21:42:26 +0800

    表单伪造csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        // 路由 api/xxxx的都放行不验证csrf
+        // 'api/*'
     ];
 }
